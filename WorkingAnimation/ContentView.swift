9835985978d6ea7f7068b5b884e62a13ac412afc
Javier Fernández <kewo_007@hotmail.com>
2020-05-04 17:35:12 +0200

    Initial Commit

---
+++ WorkingAnimation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WorkingAnimation
+//
+//  Created by Javier Fernández on 04/05/2020.
+//  Copyright © 2020 Silversun Studio. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
